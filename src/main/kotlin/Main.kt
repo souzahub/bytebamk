c0b911b69ae4d3a8ba16a6e7b73089a4e7428f92
souzahub <luansouza88@gmail.com>
2022-08-04 22:53:00 -0300

    Commit Inicial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    println("Bem Vindo ao Bytebank")
+    val titular: String = "Luan"
+    val numeroCOnta: Int = 1000
+    var saldo = 0.0
+
+    saldo = 100.0
+
+    saldo += 200
+
+
+    println("titular $titular ")
+    println("numero da conta $numeroCOnta")
+    println("saldo da conta $saldo")
+}
